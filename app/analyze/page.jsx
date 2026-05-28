55b8c6bd33eb7e50d62821ad4fcfa37376a2f356
mohammad1anvar1 <mohammad1anvar1@gmail.com>
2026-05-28 16:40:30 +0400

    Fix analyze import path

--- app/analyze/page.jsx
+++ app/analyze/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import PropertyAnalyzer from "@/components/PropertyAnalyzer";
+import PropertyAnalyzer from "../../components/PropertyAnalyzer";
 
 export default function AnalyzePage() {
   return <PropertyAnalyzer />;
