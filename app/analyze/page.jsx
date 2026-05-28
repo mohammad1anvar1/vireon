8f7f9a974a2417bad03c3898e1657b8493d6bc55
mohammad1anvar1 <mohammad1anvar1@gmail.com>
2026-05-28 19:08:44 +0400

    Fix analyze page component

--- app/analyze/page.jsx
+++ app/analyze/page.jsx
@@ -1,16 +1,7 @@
-<a
-  href="/analyze"
-  style={{
-    display: "inline-block",
-    marginTop: "20px",
-    padding: "14px 28px",
-    background: "#708238",
-    color: "white",
-    borderRadius: "10px",
-    textDecoration: "none",
-    fontWeight: "600",
-    fontSize: "18px"
-  }}
->
-  Start AI Property Preview Analysis
-</a>
+"use client";
+
+import PropertyAnalyzer from "../components/PropertyAnalyzer";
+
+export default function AnalyzePage() {
+  return <PropertyAnalyzer />;
+}
